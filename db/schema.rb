786157179d26f9646cacd844d60659ad2003e62b
Shea Munion <sheamunion@gmail.com>
2016-06-28 15:47:16 -0500

    Remove customer model

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623163316) do
-
-  create_table "customers", force: :cascade do |t|
-    t.string   "first_name",           null: false
-    t.string   "last_name",            null: false
-    t.string   "payment_method_nonce"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
